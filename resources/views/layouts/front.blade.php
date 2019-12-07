175a296431bdd5e036a3a073fa7916cf8e5246ea
”渡部亮介” <luiege35rw@yahoo.co.jp>
2019-12-07 22:58:44 +0000

    SCSS解決

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -25,7 +25,7 @@
         <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
         {{-- この章の後半で作成するCSSを読み込みます --}}
         <!--<link href="{{ secure_asset('css/admin.css') }}" rel="stylesheet">-->
-        <link href="{{ asset('css/front.css') }}" rel="stylesheet">
+        <link href="{{ secure_asset('css/front.css') }}" rel="stylesheet">
         <!--FontAwesome5を使う-->
         <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
     </head>
