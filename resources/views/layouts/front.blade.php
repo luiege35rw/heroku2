57a777f227b8af338c89d46fd63788db061bedec
”渡部亮介” <luiege35rw@yahoo.co.jp>
2020-01-06 14:48:15 +0000

    ライトボックス2実装仮終了

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -60,24 +60,24 @@
      </div>
      
      <!--ライトボックス写真一覧-->
- <a href="storage/image/bigimages1.jpg" data-lightbox="アリスト" data-title="車拡大写真"><img src="storage/image/images1_thumb.jpg" alt="アリスト"></a>
-<a href="storage/image/bigimages2.jpg" data-lightbox="クラウン210系" data-title="車拡大写真"><img src="storage/image/images2_thumb.jpg" alt="クラウン210系"></a>
-<a href="storage/image/bigimages3.jpg" data-lightbox="スカイラインR34" data-title="車拡大写真"><img src="storage/image/images3_thumb.jpg" alt="スカイラインR34"></a>
-<a href="storage/image/bigimages4.jpg" data-lightbox="チェイサー" data-title="車拡大写真"><img src="storage/image/images4_thumb.jpg" alt="チェイサー"></a>
-<a href="storage/image/bigimages5.jpg" data-lightbox="ノート" data-title="車拡大写真"><img src="storage/image/images5_thumb.jpg" alt="ノート"></a>
-<a href="storage/image/bigimages6.jpg" data-lightbox="ポルシェ911" data-title="車拡大写真"><img src="storage/image/images6_thumb.jpg" alt="ポルシェ911"></a>
-<a href="storage/image/bigimages7.jpg" data-lightbox="マツダロードスター" data-title="車拡大写真"><img src="storage/image/images7_thumb.jpg" alt="マツダロードスター"></a>
-<a href="storage/image/bigimages8.jpg" data-lightbox="ランドクルーザー" data-title="車拡大写真"><img src="storage/image/images8_thumb.jpg" alt="ランドクルーザー"></a>
-<a href="storage/image/bigimages9.jpg" data-lightbox="ワゴンR" data-title="車拡大写真"><img src="storage/image/images9_thumb.jpg" alt="ワゴンR"></a>
-<a href="storage/image/bigimages10.jpg" data-lightbox="EKワゴン" data-title="車拡大写真"><img src="storage/image/images10_thumb.jpg" alt="EKワゴン"></a>
-<a href="storage/image/bigimages11.jpg" data-lightbox="ワゴンR" data-title="車拡大写真"><img src="storage/image/images11_thumb.jpg" alt="ワゴンR"></a>
-<a href="storage/image/bigimages12.jpg" data-lightbox="ワゴンR" data-title="車拡大写真"><img src="storage/image/images12_thumb.jpg" alt="ワゴンR"></a>
-<a href="storage/image/bigimages13.jpg" data-lightbox="ワゴンR" data-title="車拡大写真"><img src="storage/image/images13_thumb.jpg" alt="ワゴンR"></a>
-<a href="storage/image/bigimages14.jpg" data-lightbox="ワゴンR" data-title="車拡大写真"><img src="storage/image/images14_thumb.jpg" alt="ワゴンR"></a>
-<a href="storage/image/bigimages15.jpg" data-lightbox="ワゴンR" data-title="車拡大写真"><img src="storage/image/images15_thumb.jpg" alt="ワゴンR"></a>
-<a href="storage/image/bigimages16.jpg" data-lightbox="ワゴンR" data-title="車拡大写真"><img src="storage/image/images16_thumb.jpg" alt="ワゴンR"></a>
-<a href="storage/image/bigimages17.jpg" data-lightbox="ワゴンR" data-title="車拡大写真"><img src="storage/image/images17_thumb.jpg" alt="ワゴンR"></a>
-<a href="storage/image/bigimages18.jpg" data-lightbox="ワゴンR" data-title="車拡大写真"><img src="storage/image/images18_thumb.jpg" alt="ワゴンR"></a>
+ <a href="storage/image/bigimages1.jpg" data-lightbox="CAR" data-title="アリスト"><img src="storage/image/images1_thumb.jpg" alt="アリスト"></a>
+<a href="storage/image/bigimages2.jpg" data-lightbox="CAR" data-title="クラウン210系"><img src="storage/image/images2_thumb.jpg" alt="クラウン210系"></a>
+<a href="storage/image/bigimages3.jpg" data-lightbox="CAR" data-title="スカイラインR34"><img src="storage/image/images3_thumb.jpg" alt="スカイラインR34"></a>
+<a href="storage/image/bigimages4.jpg" data-lightbox="CAR" data-title="チェイサー"><img src="storage/image/images4_thumb.jpg" alt="チェイサー"></a>
+<a href="storage/image/bigimages5.jpg" data-lightbox="CAR" data-title="ノート"><img src="storage/image/images5_thumb.jpg" alt="ノート"></a>
+<a href="storage/image/bigimages6.jpg" data-lightbox="CAR" data-title="ポルシェ911"><img src="storage/image/images6_thumb.jpg" alt="ポルシェ911"></a>
+<a href="storage/image/bigimages7.jpg" data-lightbox="CAR" data-title="マツダロードスター"><img src="storage/image/images7_thumb.jpg" alt="マツダロードスター"></a>
+<a href="storage/image/bigimages8.jpg" data-lightbox="CAR" data-title="ランドクルーザー"><img src="storage/image/images8_thumb.jpg" alt="ランドクルーザー"></a>
+<a href="storage/image/bigimages9.jpg" data-lightbox="CAR" data-title="ワゴンR"><img src="storage/image/images9_thumb.jpg" alt="ワゴンR"></a>
+<a href="storage/image/bigimages10.jpg" data-lightbox="CAR" data-title="EKワゴン"><img src="storage/image/images10_thumb.jpg" alt="EKワゴン"></a>
+<a href="storage/image/bigimages11.jpg" data-lightbox="CAR" data-title="プリウス"><img src="storage/image/images11_thumb.jpg" alt="プリウス"></a>
+<a href="storage/image/bigimages12.jpg" data-lightbox="CAR" data-title="ピンククラウン"><img src="storage/image/images12_thumb.jpg" alt="ピンククラウン"></a>
+<a href="storage/image/bigimages13.jpg" data-lightbox="CAR" data-title="マセラティギブリ"><img src="storage/image/images13_thumb.jpg" alt="マセラティギブリ"></a>
+<a href="storage/image/bigimages14.jpg" data-lightbox="CAR" data-title="ベンツAMG-GT"><img src="storage/image/images14_thumb.jpg" alt="ベンツAMG-GT"></a>
+<a href="storage/image/bigimages15.jpg" data-lightbox="CAR" data-title="フェラーリスパイダー"><img src="storage/image/images15_thumb.jpg" alt="フェラーリスパイダー"></a>
+<a href="storage/image/bigimages16.jpg" data-lightbox="CAR" data-title="大黒ふ頭"><img src="storage/image/images16_thumb.jpg" alt="大黒ふ頭"></a>
+<a href="storage/image/bigimages17.jpg" data-lightbox="CAR" data-title="ピンククラウン納車"><img src="storage/image/images17_thumb.jpg" alt="ピンククラウン納車"></a>
+<a href="storage/image/bigimages18.jpg" data-lightbox="CAR" data-title="千葉フォルニア"><img src="storage/image/images18_thumb.jpg" alt="千葉フォルニア"></a>
 
 
             {{-- ここまでナビゲーションバー --}}
