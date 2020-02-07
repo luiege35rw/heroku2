2a032810797dfbe4f2ea7aec0bf48fe5f0ee357b
EC2 Default User <ec2-user@ip-172-31-32-157.us-east-2.compute.internal>
2020-02-07 13:08:29 +0000

    2月仮デプロイ12

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -3,46 +3,50 @@
 @section('content')
     <div class="container">
         <hr color="#c0c0c0">
-        <div class="row">
-            <div class="posts col-md-8 mx-auto mt-3">
+        <div class="row2">
+            <div class="posts col-md-8 mx-auto ">
                 @foreach($posts as $post)
-                    <div class="post">
-                        <div class="row">
-                            <div class="text col-md-6">
-                                <div class="date">
-                                    {{ $post->updated_at->format('Y年m月d日') }}
-                                </div>
-                                <div class="name mt-3">
-                                    {{ str_limit($post->name, 1500) }}
-                                </div>
-                                <div class="modelhistory mt-3">
-                                    {{ str_limit($post->modelhistory, 1500) }}
-                                </div>
-                                <div class="spot mt-3">
-                                    {{ str_limit($post->spot, 1500) }}
-                                </div>
-                                 <div class="introduction mt-3">
-                                    {{ str_limit($post->introduction, 1500) }}
-                                </div>
-                            </div>
-                            <div class="image col-md-6 text-right md-4">
-                              @if ($post->image_path)
-                                    <img src="{{ $post->image_path }}" style="max-width:300px;max-hight:200px;">
-                                 @endif
-                             </div>
-                        </div>
-                        <div>
-                            @for ($i =1; $i <= 6; $i++)
-                            @if ($post->{'image_path' . $i})
-                                <img src="{{ $post->{'image_path' . $i} }}" style="max-width:300px;max-hight:200px;">
-                            @endif
-                            @endfor
-                        </div>
+            <div class="post">
+            <div class="row2">
+                            
+                        <!--プロフィールアバター写真投稿-->
+             <div class="image image2-top  text-center mt-4">
+                 @if ($post->image_path)
+                 　　<a href="{{ $post->image_path }}" data-lightbox="car">
+                         <img src="{{ $post->image_path }}" style="max-width:300px;max-hight:200px">
+                     </a>
+                      <p class="cg"><p><p>{{ str_limit($post->name, 150) }}</p></p>
+                 @endif
+             </div>
+            </div>
+                            
+                            
+                            
+                          <!--プロフィール写真投稿ループ-->
+            <div class=" mx-auto mt-4">
+                @for ($i =1; $i <= 6; $i++)
+                @if ($post->{'image_path' . $i})
+                <a href="{{ $post->{'image_path' . $i} }}" data-lightbox="car">
+                    <img src="{{ $post->{'image_path' . $i} }}" style="max-width:300px;max-hight:200px">
+                </a>
+                @endif
+                @endfor
+            </div>
+            
+                            <!--プロフィール情報を表示する-->
+             <div class="text-center mt-4">
+                    {{ str_limit($post->modelhistory, 150) }}
+             </div>
+             <div class="text-center mt-4">
+                    {{ str_limit($post->spot, 150) }}
+             </div>
+             <div class="text-center mt-4">
+                    {{ str_limit($post->introduction, 1500) }}
+             </div>
                         
                         
                         
-                        
-                    </div>
+          
                     <hr color="#c0c0c0">
                 @endforeach
             </div>
